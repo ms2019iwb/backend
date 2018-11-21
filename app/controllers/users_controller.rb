6abb1caa0b0200f54c2616447311347f3f18204e
hirotowatanabe <hiroto.private@gmail.com>
2018-11-21 17:36:29 +0900

    Add users#create

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+end
